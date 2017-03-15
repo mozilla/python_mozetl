484048b2f5b3dae303f7f8c277d41ae1a717ba8a
Ryan Harter <harterrt@gmail.com>
2017-03-15 17:21:38 -0400

    Add periodicity option to airflow script

--- python_etl/main.py
+++ python_etl/main.py
@@ -1,40 +1,12 @@
-from datetime import date, timedelta
-from pyspark import Row
+import example_job
 
-from moztelemetry import get_pings_properties
-from moztelemetry.dataset import Dataset
+def weekly(sc, sqlContext):
+    pass
 
+def daily(sc, sqlContext):
+    example_job.etl_job(sc, sqlContext)
+    example_job.etl_job(sc, sqlContext)
+    example_job.etl_job(sc, sqlContext)
+    print "carpe diem"
 
-def get_data(sc):
-    pings = Dataset.from_source("telemetry") \
-        .where(docType='main') \
-        .where(submissionDate=(date.today() - timedelta(1)).strftime("%Y%m%d")) \
-        .where(appUpdateChannel="nightly") \
-        .records(sc, sample=0.1)
-
-    return get_pings_properties(pings, ["clientId",
-                                        "environment/system/os/name"])
-
-def ping_to_row(ping):
-    return Row(client_id = ping['clientId'],
-               os = ping['environment/system/os/name'])
-
-def transform_pings(pings):
-    """Take a dataframe of main pings and summarize OS share"""
-    out = pings.map(ping_to_row).distinct()\
-        .map(lambda x: x.os).countByValue()
-
-    return dict(out)
-
-def etl_job(sc, sqlContext):
-    """This is the function that will be executed on the cluster"""
-
-    results = transform_pings(get_data(sc))
-
-    # Display results:
-    total = sum(map(lambda x: x[1], results.items()))
-    # Print the OS and client_id counts in descending order:
-    for pair in sorted(results.items(), key=lambda x: -x[1]):
-        print "OS: {:<10} Percent: {:0.2f}%"\
-            .format(pair[0], float(pair[1])/total*100)
-
+jobs = [example_job.etl_job]*3
--- scheduling/airflow.py
+++ scheduling/airflow.py
@@ -1,10 +1,24 @@
+import argparse
+
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 from python_etl import main
 
 if __name__ == "__main__":
+    # Figure out what group of functions we're running
+    periodicity_functions = {
+        'daily': main.daily,
+        'weekly': main.weekly,
+    }
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('periodicity', choices=periodicity_functions.keys())
+    args = parser.parse_args()
+
+    # Initialize Spark objects
     conf = SparkConf().setAppName('python_etl')
     sc = SparkContext(conf=conf)
     sqlContext = SQLContext(sc)
 
-    main.etl_job(sc, sqlContext)
+    # Run
+    periodicity_functions[args.periodicity](sc, sqlContext)
