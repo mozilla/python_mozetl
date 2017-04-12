394d32b2ef530bec1c39582da0777dcf75f62756
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2017-04-11 17:06:21 -0700

    Add .travis.yml and tox.ini for automated testing
    This uses tox for setting up the environment and test-runner. The
    dependencies for this are added to setup.py if you want to run pytest
    manually with your own virtual environment, while tox is used for
    automatically setting up the environment.
    
    This will run tests with dependencies on spark on travis, and then send
    the results to codecov.io.

--- setup.py
+++ setup.py
@@ -10,4 +10,12 @@ setup(name='python_etl',
       author_email='harterrt@mozilla.com',
       url='https://github.com/mozilla/python_etl.git',
       packages=find_packages(exclude=['tests']),
-)
+      install_requires=[
+          'python_moztelemetry',
+          ],
+      test_requires=[
+          'pytest',
+          'coverage',
+          'pytest-cov'
+          ]
+      )
