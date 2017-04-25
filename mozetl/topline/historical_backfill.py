cfefed7d108c8900f24cb2ef6dd50a22f2b1e506
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2017-04-25 14:43:00 -0700

    Bug 1359193 - Add output schema and backfill implementation

--- mozetl/topline/historical_backfill.py
+++ mozetl/topline/historical_backfill.py
@@ -1,22 +1,28 @@
-import click
 import json
+import logging
 import os
-from pyspark.sql import SparkSession
+
+import click
+
+from pyspark.sql import SparkSession, functions as F
 from pyspark.sql.types import StructType
 
 
-def schema_from_json(path):
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def schema_from_json(path, relative=True):
+    if relative:
+        path = os.path.join(os.path.dirname(__file__), path)
+
     with open(path) as json_data:
         data = json.load(json_data)
     return StructType.fromJson(data)
 
 
-historical_schema = schema_from_json(
-    os.path.join(
-        os.path.dirname(__file__),
-        'historical_schema.json'
-    )
-)
+historical_schema = schema_from_json('historical_schema.json')
+topline_schema = schema_from_json('topline_schema.json')
 
 
 def create_s3_path(bucket, prefix, protocol="s3"):
@@ -24,7 +30,23 @@ def create_s3_path(bucket, prefix, protocol="s3"):
 
 
 def backfill_topline_summary(historical_df, path):
-    historical_df.write.parquet(path)
+    logging.info("Saving historical data to {}.".format(path))
+
+    df = (
+        historical_df
+        .where(
+              (F.col('geo') != 'all') &
+              (F.col('os') != 'all') &
+              (F.col('channel') != 'all'))
+        .withColumn('report_start', F.date_format('date', 'yyyyMMdd'))
+    )
+
+    # Cast all elements from the csv file. Assume both schemas are flat.
+    df = df.select(*[F.col(f.name).cast(f.dataType).alias(f.name)
+                     for f in topline_schema.fields])
+
+    # Use the same parititoning scheme as topline_summary
+    df.write.partitionBy('report_start').parquet(path)
 
 
 @click.command()
@@ -37,11 +59,17 @@ def main(s3_path, mode, bucket, prefix):
              .builder
              .appName('topline_historical_backfill')
              .getOrCreate())
+
+    logging.info("Running historical backfill for {} executive report at {}."
+                 .format(mode, s3_path))
+
     historical_df = spark.read.csv(s3_path,
                                    schema=historical_schema,
                                    header=True)
     output_path = create_s3_path(bucket, '{}/mode={}'.format(prefix, mode))
     backfill_topline_summary(historical_df, output_path)
+
+    logging.info("Finished historical backfill job.")
 
 
 if __name__ == '__main__':
