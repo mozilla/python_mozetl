3ab6a9890ba4e44ac5bf8d105515e5057c413a40
Jannis Leidel <jannis@leidel.info>
2017-04-25 16:14:15 +0200

    Various code smell and small speed improvments.
    Uses operator.itemgetter, iteritems, new style classes.
    Moves some functions outside of their user functions to not have to create the inner function on every call.

--- python_etl/basic_etl/transform.py
+++ python_etl/basic_etl/transform.py
@@ -1,20 +1,23 @@
-from pyspark.sql.types import StructType, StructField
 from collections import namedtuple
+
 from moztelemetry import get_pings_properties
+from pyspark.sql.types import StructType, StructField
+
 
 ColumnConfig = namedtuple('ColumnConfig',
                           ['name', 'path', 'cleaning_func', 'struct_type'])
 
 
-class DataFrameConfig:
+class DataFrameConfig(object):
     def __init__(self, col_configs, ping_filter):
         self.columns = [ColumnConfig(*col) for col in col_configs]
         self.ping_filter = ping_filter
 
     def toStructType(self):
-        return StructType(map(
-            lambda col: StructField(col.name, col.struct_type, True),
-            self.columns))
+        return StructType(
+            map(lambda col: StructField(col.name, col.struct_type, True),
+                self.columns)
+        )
 
     def get_paths(self):
         return map(lambda col: col.path, self.columns)
@@ -22,10 +25,25 @@ class DataFrameConfig:
 
 def convert_pings(sqlContext, pings, data_frame_config):
     """Performs basic data pipelining on raw telemetry pings """
-    filtered_pings = get_pings_properties(pings, data_frame_config.get_paths())\
-        .filter(data_frame_config.ping_filter)
+    filtered_pings = get_pings_properties(
+        pings,
+        data_frame_config.get_paths()
+    ).filter(data_frame_config.ping_filter)
 
     return convert_rdd(sqlContext, filtered_pings, data_frame_config)
+
+
+def _build_cell(ping, column_config):
+    """Take a json ping and a ColumnConfig and return a cleaned cell"""
+    raw_value = ping[column_config.path]
+    func = column_config.cleaning_func
+    if func is not None:
+        try:
+            return func(raw_value)
+        except:  # TODO: what exception could happen here, fill in?
+            return None
+    else:
+        return raw_value
 
 
 def convert_rdd(sqlContext, raw_data, data_frame_config):
@@ -33,20 +51,9 @@ def convert_rdd(sqlContext, raw_data, data_frame_config):
 
     raw_data must have a map method.
     """
-    def build_cell(ping, column_config):
-        """Take a json ping and a ColumnConfig and return a cleaned cell"""
-        raw_value = ping[column_config.path]
-        func = column_config.cleaning_func
-        if func is not None:
-            try:
-                return func(raw_value)
-            except:
-                return None
-        else:
-            return raw_value
 
     def ping_to_row(ping):
-        return [build_cell(ping, col) for col in data_frame_config.columns]
+        return [_build_cell(ping, col) for col in data_frame_config.columns]
 
     return sqlContext.createDataFrame(
         raw_data.map(ping_to_row),
