bd548e2f050831bdd8bbef887464cb9229fc738b
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2017-05-03 18:36:07 +0000

    Fixes #22 - Add static files to setup.py
    This fixes issues involved with deployment via bdist_egg.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='harterrt@mozilla.com',
     url='https://github.com/mozilla/python_mozetl.git',
     packages=find_packages(exclude=['tests']),
+    include_package_data=True,
     install_requires=[
         'python_moztelemetry',  # TODO: pin version
         'click',
