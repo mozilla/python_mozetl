bdb7dfd97f22601a2104bcc03bf26b704e77bc9b
groovecoder <luke.crouch@gmail.com>
2017-05-30 14:44:37 -0500

    conslidate txp & shield containers code

--- mozetl/testpilot/containers.py
+++ mozetl/testpilot/containers.py
@@ -4,31 +4,62 @@ from ..basic import convert_pings, DataFrameConfig
 from .utils import testpilot_etl_boilerplate
 
 
-def transform_pings(sqlContext, pings):
+TESTPILOT_ADDON_ID = "@testpilot-containers"
+SHIELD_ADDON_ID = "@shield-study-containers"
+DATAFRAME_COLUMN_CONFIGS = [
+    ("uuid", "payload/payload/uuid", None, StringType()),
+    ("userContextId", "payload/payload/userContextId", None, StringType()),
+    ("clickedContainerTabCount",
+     "payload/payload/clickedContainerTabCount", None, LongType()),
+    ("eventSource", "payload/payload/eventSource", None, StringType()),
+    ("event", "payload/payload/event", None, StringType()),
+    ("hiddenContainersCount", "payload/payload/hiddenContainersCount", None, LongType()),
+    ("shownContainersCount", "payload/payload/shownContainersCount", None, LongType()),
+    ("totalContainersCount", "payload/payload/totalContainersCount", None, LongType()),
+    ("totalContainerTabsCount",
+     "payload/payload/totalContainerTabsCount", None, LongType()),
+    ("totalNonContainerTabsCount",
+     "payload/payload/totalNonContainerTabsCount", None, LongType()),
+    ("pageRequestCount", "payload/payload/pageRequestCount", None, LongType()),
+    ("test", "payload/test", None, StringType()),
+]
+
+
+def transform_testpilot_pings(sqlContext, pings):
     return convert_pings(
         sqlContext,
         pings,
-        DataFrameConfig([
-            ("uuid", "payload/payload/uuid", None, StringType()),
-            ("userContextId", "payload/payload/userContextId", None, StringType()),
-            ("clickedContainerTabCount",
-             "payload/payload/clickedContainerTabCount", None, LongType()),
-            ("eventSource", "payload/payload/eventSource", None, StringType()),
-            ("event", "payload/payload/event", None, StringType()),
-            ("hiddenContainersCount", "payload/payload/hiddenContainersCount", None, LongType()),
-            ("shownContainersCount", "payload/payload/shownContainersCount", None, LongType()),
-            ("totalContainersCount", "payload/payload/totalContainersCount", None, LongType()),
-            ("totalContainerTabsCount",
-             "payload/payload/totalContainerTabsCount", None, LongType()),
-            ("totalNonContainerTabsCount",
-             "payload/payload/totalNonContainerTabsCount", None, LongType()),
-            ("pageRequestCount", "payload/payload/pageRequestCount", None, LongType()),
-            ("test", "payload/test", None, StringType()),
-        ], lambda ping: ping['payload/test'] == "@testpilot-containers")
+        DataFrameConfig(DATAFRAME_COLUMN_CONFIGS, include_testpilot_pings)
     )
 
 
-etl_job = testpilot_etl_boilerplate(
-    transform_pings,
-    's3n://telemetry-parquet/harter/containers_testpilottest/v1'
+def transform_shield_pings(sqlContext, pings):
+    return convert_pings(
+        sqlContext,
+        pings,
+        DataFrameConfig(DATAFRAME_COLUMN_CONFIGS, include_testpilot_and_shield_pings)
+    )
+
+
+def include_testpilot_pings(ping):
+    return ping['payload/test'] == TESTPILOT_ADDON_ID
+
+
+def include_testpilot_and_shield_pings(ping):
+    return ping['payload/test'] in [TESTPILOT_ADDON_ID, SHIELD_ADDON_ID]
+
+
+testpilot_etl_job = testpilot_etl_boilerplate(
+    transform_testpilot_pings,
+    's3n://telemetry-parquet/harter/containers_testpilottest/v2'
 )
+
+shield_etl_job = testpilot_etl_boilerplate(
+    transform_shield_pings,
+    's3n://telemetry-parquet/harter/containers_shield/v1'
+)
+
+
+def etl_job():
+    testpilot_etl_job()
+    shield_etl_job()
