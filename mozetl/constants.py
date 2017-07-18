2d0f42b141a05d2466636b94164399e5677110d0
Andrei Oprea <andrei.br92@gmail.com>
2017-07-18 15:34:34 +0200

    Bug 1381460 - Whitelist activity stream event source

--- mozetl/constants.py
+++ mozetl/constants.py
@@ -3,5 +3,6 @@
 # https://bugzilla.mozilla.org/show_bug.cgi?id=1367554
 # https://bugzilla.mozilla.org/show_bug.cgi?id=1368089
 SEARCH_SOURCE_WHITELIST = [
-    'searchbar', 'urlbar', 'abouthome', 'newtab', 'contextmenu', 'system'
+    'searchbar', 'urlbar', 'abouthome', 'newtab', 'contextmenu', 'system',
+    'activitystream'
 ]
