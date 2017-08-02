5feadfd591f67284f6c0208ae783a82ffc7349ed
groovecoder <luke.crouch@gmail.com>
2017-08-02 11:27:09 -0500

    bug 1386446: fix shield doc type

---
+++ mozetl/shield/utils.py
@@ -0,0 +1,27 @@
+from datetime import date, timedelta
+from moztelemetry.dataset import Dataset
+
+
+def shield_etl_boilerplate(transform_func, s3_path):
+    def etl_job(sc, sqlContext, submission_date=None, save=True):
+        if submission_date is None:
+            submission_date = (date.today() - timedelta(1)).strftime("%Y%m%d")
+
+        pings = Dataset.from_source(
+            "telemetry"
+        ).where(
+            docType="shield-study",
+            submissionDate=submission_date,
+            appName="Firefox",
+        ).records(sc)
+
+        transformed_pings = transform_func(sqlContext, pings)
+
+        if save:
+            # path = 's3://telemetry-parquet/testpilot/txp_pulse/v1/submission_date={}'
+            path = s3_path + '/submission_date={}'.format(submission_date)
+            transformed_pings.repartition(1).write.mode('overwrite').parquet(path)
+
+        return transformed_pings
+
+    return etl_job
