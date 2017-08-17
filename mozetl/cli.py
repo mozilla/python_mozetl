d0ec6e926454347d4db82d4875054827c8e102df
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2017-08-17 13:55:39 -0700

    Bug 1374831 - Reformat bookmark validation gist and add to entrypoint

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -2,6 +2,7 @@ import click
 from mozetl.churn import churn
 from mozetl.search import dashboard, search_rollups
 from mozetl.taar import taar_locale
+from mozetl.sync import bookmark_validation
 
 
 @click.group()
@@ -13,3 +14,4 @@ entry_point.add_command(churn.main, "churn")
 entry_point.add_command(dashboard.main, "search_dashboard")
 entry_point.add_command(search_rollups.main, "search_rollup")
 entry_point.add_command(taar_locale.main, "taar_locale")
+entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
