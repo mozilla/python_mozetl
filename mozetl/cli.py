70c74b952ca1a91d5b94d189a380668dfdccba5c
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2017-08-22 14:56:22 -0700

    Bug 1365580 - Add search_rollups to the cli

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -1,6 +1,6 @@
 import click
 from mozetl.churn import churn
-from mozetl.search import dashboard
+from mozetl.search import dashboard, search_rollups
 
 
 @click.group()
@@ -10,3 +10,4 @@ def entry_point():
 
 entry_point.add_command(churn.main, "churn")
 entry_point.add_command(dashboard.main, "search_dashboard")
+entry_point.add_command(search_rollups.main, "search_rollup")
