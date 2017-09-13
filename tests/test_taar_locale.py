14073ac73e4335f9ad5d3fe320ca875ce5dc896c
mlopatka <mlopatka>
2017-09-13 16:10:27 -0700

    added test for legacy addon filtering

--- tests/test_taar_locale.py
+++ tests/test_taar_locale.py
@@ -1,6 +1,7 @@
 """Test suite for TAAR Locale Job."""
 
 import boto3
+import json
 import functools
 import pytest
 from moto import mock_s3
@@ -57,6 +58,56 @@ default_sample = {
         }
       }
     ]
+}
+
+fake_amo_sample_web_extension = {
+    'test-addon-key':
+        {'ratings':
+             {'bayesian_average': 0.0,
+              'count': 0.0,
+              'average': 0.0},
+         'name':
+             {'en-US': 'test-amo-entry-1'},
+         'default_locale': 'en-US',
+         'tags': [
+             'test-tag-1',
+             'test-tag-2',
+             'test-tag-3'],
+         'summary': {
+             'en-US':
+                 'helps test taar_locale moz_etl job'},
+         'current_version': {
+             'files': [
+                 {'status': 'public',
+                  'platform': 'all',
+                  'id': 000001,
+                  'is_webextension': True}]},
+         'guid': 'test-guid-0001'}
+}
+
+fake_amo_sample_legacy = {
+    'test-addon-key':
+        {'ratings':
+             {'bayesian_average': 0.0,
+              'count': 0.0,
+              'average': 0.0},
+         'name':
+             {'en-US': 'test-amo-entry-2'},
+         'default_locale': 'en-US',
+         'tags': [
+             'test-tag-1',
+             'test-tag-2',
+             'test-tag-3'],
+         'summary': {
+             'en-US':
+                 'helps test taar_locale moz_etl job'},
+         'current_version': {
+             'files': [
+                 {'status': 'public',
+                  'platform': 'all',
+                  'id': 000002,
+                  'is_webextension': False}]},
+         'guid': 'test-guid-0002'}
 }
 
 
@@ -130,3 +181,27 @@ def test_write_output():
     date_filename =\
         "{}{}20171106.json".format(prefix, taar_locale.LOCALE_FILE_NAME)
     assert date_filename in keys
+
+
+@mock_s3
+def test_load_amo_external_whitelist():
+    amo_test = taar_locale.load_amo_external_whitelist()
+    bucket = 'test-bucket'
+    prefix = 'test-prefix/'
+
+    content = [fake_amo_sample_web_extension, fake_amo_sample_legacy]
+
+    conn = boto3.resource('s3', region_name='us-west-2')
+    bucket_obj = conn.create_bucket(Bucket=bucket)
+
+    # Store the data in the mocked bucket.
+    taar_locale.store(json.dumps(content), prefix, bucket)
+
+    # Get the content of the bucket.
+    available_objects = list(bucket_obj.objects.filter(Prefix=prefix))
+    assert len(available_objects) == 2
+
+    # Check that the web_extension item is still present and the legacy addon is absent.
+    white_listed_addons = taar_locale.load_amo_external_whitelist()
+    assert 'test-guid-0002' in white_listed_addons
+    assert 'test-guid-0001' not in white_listed_addons
