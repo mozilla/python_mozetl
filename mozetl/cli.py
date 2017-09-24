e86e36edf2321155b45427fa0b8d3fe24954d9ea
Mark Reid <mreid@mozilla.com>
2017-09-24 19:21:50 -0300

    Add cli entry point for clients/experiments daily

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -3,6 +3,8 @@ from mozetl.churn import churn
 from mozetl.search import dashboard, search_rollups
 from mozetl.taar import taar_locale, taar_similarity
 from mozetl.sync import bookmark_validation
+from mozetl.clientsdaily import rollup as clientsdaily
+from mozetl.experimentsdaily import rollup as experimentsdaily
 
 
 @click.group()
@@ -11,8 +13,13 @@ def entry_point():
 
 
 entry_point.add_command(churn.main, "churn")
+entry_point.add_command(clientsdaily.main, "clients_daily")
+entry_point.add_command(experimentsdaily.main, "experiments_daily")
 entry_point.add_command(dashboard.main, "search_dashboard")
 entry_point.add_command(search_rollups.main, "search_rollup")
+entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
 entry_point.add_command(taar_locale.main, "taar_locale")
 entry_point.add_command(taar_similarity.main, "taar_similarity")
-entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
+
+if __name__ == '__main__':
+	entry_point()
