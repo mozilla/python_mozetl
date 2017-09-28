1a51308cb4b09f5ecc4f575dc99e023d4c00a954
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2017-09-28 16:59:35 -0700

    Bug 1381840 - Add 1 day Firefox retention intermediate dataset
    In it's current state, this should be considered `retention_dev_v3`. The
    link to the STMO example dashboard for related datasets can be found in
    [1].
    
    This contains the cleaning routines for `main_summary` and `new_profile`
    for use in a 1 day retention day dataset. There can be several instances
    of the same client across a day (on average ~3 pings per client). This
    is later processed by aggregating using HyperLogLog UDF's in
    Telemetry-Batch-View.
    
    This contains necessary columns for calculating retention by
    attribution over profile counts and for calculating engagement over
    subpopulations.
    
    The extract routine is borrowed from churn, while transform uses the
    same subroutines for cleaning and transforming the dataset.
    
    [1]
    https://sql.telemetry.mozilla.org/dashboard/firefox-telemetry-retention-dataset-example-usage

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -5,6 +5,7 @@ from mozetl.taar import taar_locale, taar_similarity
 from mozetl.sync import bookmark_validation
 from mozetl.clientsdaily import rollup as clientsdaily
 from mozetl.experimentsdaily import rollup as experimentsdaily
+from mozetl.retention import daily_retention
 
 
 @click.group()
@@ -15,6 +16,7 @@ def entry_point():
 entry_point.add_command(churn.main, "churn")
 entry_point.add_command(clientsdaily.main, "clients_daily")
 entry_point.add_command(experimentsdaily.main, "experiments_daily")
+entry_point.add_command(daily_retention.main, "retention")
 entry_point.add_command(dashboard.main, "search_dashboard")
 entry_point.add_command(search_rollups.main, "search_rollup")
 entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
