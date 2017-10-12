af39682e1e232357f1e89a3031cfdf4a674cf6b1
mlopatka <mlopatka@mozilla.com>
2017-10-12 19:44:50 +0200

    Add taar_legacy to cli.py

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -6,7 +6,7 @@ from mozetl.engagement.retention import job as retention_job
 from mozetl.experimentsdaily import rollup as experimentsdaily
 from mozetl.search import dashboard, search_rollups
 from mozetl.sync import bookmark_validation
-from mozetl.taar import taar_locale, taar_similarity
+from mozetl.taar import taar_locale, taar_similarity, taar_legacy
 
 
 @click.group()
@@ -23,6 +23,7 @@ entry_point.add_command(search_rollups.main, "search_rollup")
 entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
 entry_point.add_command(taar_locale.main, "taar_locale")
 entry_point.add_command(taar_similarity.main, "taar_similarity")
+entry_point.add_command(taar_legacy.main, "taar_legacy")
 
 if __name__ == '__main__':
     entry_point()
