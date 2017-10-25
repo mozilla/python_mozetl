8db901d43a966d3329a35fb3280cd07d9ab30a57
Ryan Harter <harterrt@gmail.com>
2017-10-25 16:18:51 -0400

    Add experiments to cli entrypoint

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -4,7 +4,7 @@ from mozetl.clientsdaily import rollup as clientsdaily
 from mozetl.engagement.churn import job as churn_job
 from mozetl.engagement.retention import job as retention_job
 from mozetl.experimentsdaily import rollup as experimentsdaily
-from mozetl.search import dashboard, search_rollups
+from mozetl.search import dashboard, search_rollups, experiments
 from mozetl.sync import bookmark_validation
 from mozetl.taar import taar_locale, taar_similarity, taar_legacy
 
@@ -19,6 +19,7 @@ entry_point.add_command(clientsdaily.main, "clients_daily")
 entry_point.add_command(experimentsdaily.main, "experiments_daily")
 entry_point.add_command(retention_job.main, "retention")
 entry_point.add_command(dashboard.main, "search_dashboard")
+entry_point.add_command(experiments.main, "search_experiments")
 entry_point.add_command(search_rollups.main, "search_rollup")
 entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
 entry_point.add_command(taar_locale.main, "taar_locale")
