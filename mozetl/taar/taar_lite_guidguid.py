b22334d4af9443919228e76f3862257a2e7d48bf
Victor Ng <victor@crankycoder.com>
2018-03-27 09:13:55 -0400

    test commit

--- mozetl/taar/taar_lite_guidguid.py
+++ mozetl/taar/taar_lite_guidguid.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """
 This ETL job computes the co-installation occurrence of white-listed
 Firefox webextensions for a sample of the longitudinal telemetry dataset.
