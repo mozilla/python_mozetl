98cc25be359cc22fa9efeed6079d6f96277e64b2
Victor Ng <victor@crankycoder.com>
2018-03-28 10:12:59 -0400

    Reuse the existing store_json_to_s3 code in taar-utils

--- mozetl/taar/taar_lite_guidguid.py
+++ mozetl/taar/taar_lite_guidguid.py
@@ -12,6 +12,7 @@ from botocore.exceptions import ClientError   # noqa
 from pyspark.sql import Row, SparkSession     # noqa
 from pyspark.sql.functions import col, collect_list, explode, udf, sum as sum_, max as max_, first  # noqa
 from pyspark.sql.types import StructType, StructField, StringType, LongType, IntegerType  # noqa
+from taar_utils import store_json_to_s3
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -25,48 +26,6 @@ AMO_DUMP_BUCKET = 'telemetry-parquet'
 AMO_DUMP_KEY = 'telemetry-ml/addon_recommender/addons_database.json'
 MAIN_SUMMARY_PATH = 's3://telemetry-parquet/main_summary/v4/'
 ONE_WEEK_AGO = (dt.datetime.now() - dt.timedelta(days=7)).strftime('%Y%m%d')
-
-
-def write_to_s3(source_file_name, s3_dest_file_name, s3_prefix, bucket):
-    """Store the new json file containing current top addons per locale to S3.
-
-    :param source_file_name: The name of the local source file.
-    :param s3_dest_file_name: The name of the destination file on S3.
-    :param s3_prefix: The S3 prefix in the bucket.
-    :param bucket: The S3 bucket.
-    """
-    client = boto3.client('s3', 'us-west-2')
-    transfer = boto3.s3.transfer.S3Transfer(client)
-
-    # Update the state in the analysis bucket.
-    key_path = s3_prefix + s3_dest_file_name
-    transfer.upload_file(source_file_name, bucket, key_path)
-
-
-def store_json_to_s3(json_data, base_filename, date, prefix, bucket):
-    """Saves the JSON data to a local file and then uploads it to S3.
-
-    Two copies of the file will get uploaded: one with as "<base_filename>.json"
-    and the other as "<base_filename><YYYYMMDD>.json" for backup purposes.
-
-    :param json_data: A string with the JSON content to write.
-    :param base_filename: A string with the base name of the file to use for saving
-        locally and uploading to S3.
-    :param date: A date string in the "YYYYMMDD" format.
-    :param prefix: The S3 prefix.
-    :param bucket: The S3 bucket name.
-    """
-    FULL_FILENAME = "{}.json".format(base_filename)
-
-    with open(FULL_FILENAME, "w+") as json_file:
-        json_file.write(json_data)
-
-    archived_file_copy =\
-        "{}{}.json".format(base_filename, date)
-
-    # Store a copy of the current JSON with datestamp.
-    write_to_s3(FULL_FILENAME, archived_file_copy, prefix, bucket)
-    write_to_s3(FULL_FILENAME, FULL_FILENAME, prefix, bucket)
 
 
 # TODO: eventually replace this with the whitelist that Victor is writing ETL for.
