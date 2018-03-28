fc497a05014f7eb5b240b09e0a39c626a871d2ac
Victor Ng <victor@crankycoder.com>
2018-03-28 10:46:50 -0400

    Cleaned up expected MOCK_ADDON_INSTALLATIONS

--- tests/test_taar_lite_guidguid.py
+++ tests/test_taar_lite_guidguid.py
@@ -23,17 +23,31 @@ MOCK_TELEMETRY_SAMPLE = [
     Row(installed_addons=["test-guid-1", "test-guid-1"])
 ]
 
-MOCK_ADDON_INSTALLATIONS = {
-    "test-guid-1":
-        {"test-guid-2": 1,
-         "test-guid-3": 2,
-         "test-guid-4": 2
-         },
-    "test-guid-2":
-        {"test-guid-1": 2,
-         "test-guid-5": 1,
-         "test-guid-6": 1
-         }}
+MOCK_ADDON_INSTALLATIONS = [
+        (
+            Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-2', 'test-guid-3', 'test-guid-4']),
+            [Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1', 'test-guid-5', 'test-guid-6']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1'])]
+        ),
+        (
+            Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-3', 'test-guid-4']),
+            [Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-2', 'test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1', 'test-guid-5', 'test-guid-6']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1'])]
+        ),
+        (
+            Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1', 'test-guid-5', 'test-guid-6']),
+            [Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-2', 'test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1'])]
+        ),
+        (
+            Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1']),
+            [Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-2', 'test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-1', coinstalled_addons=['test-guid-3', 'test-guid-4']),
+             Row(key_addon='test-guid-2', coinstalled_addons=['test-guid-1', 'test-guid-5', 'test-guid-6'])]
+        )]
 
 MOCK_KEYED_ADDONS = [
     Row(key_addon='test-guid-1',
