2224808b3b4e017ec470811adcaff6755952a972
Victor Ng <victor@crankycoder.com>
2018-10-25 10:05:32 -0400

    Reformatted code with black (#264)

--- mozetl/taar/taar_similarity.py
+++ mozetl/taar/taar_similarity.py
@@ -25,8 +25,13 @@ from taar_utils import load_amo_curated_whitelist
 
 # Define the set of feature names to be used in the donor computations.
 CATEGORICAL_FEATURES = ["geo_city", "locale", "os"]
-CONTINUOUS_FEATURES = \
-    ["subsession_length", "bookmark_count", "tab_open_count", "total_uri", "unique_tlds"]
+CONTINUOUS_FEATURES = [
+    "subsession_length",
+    "bookmark_count",
+    "tab_open_count",
+    "total_uri",
+    "unique_tlds",
+]
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -51,9 +56,11 @@ def get_samples(spark):
             "settings[0].locale as locale",
             "os as os",
             "places_bookmarks_count[0].sum AS bookmark_count",
-            "scalar_parent_browser_engagement_tab_open_event_count[0].value AS tab_open_count",
+            "scalar_parent_browser_engagement_tab_open_event_count[0].value "
+            "AS tab_open_count",
             "scalar_parent_browser_engagement_total_uri_count[0].value AS total_uri",
-            "scalar_parent_browser_engagement_unique_domains_count[0].value AS unique_tlds"
+            "scalar_parent_browser_engagement_unique_domains_count[0].value "
+            "AS unique_tlds",
         )
     )
 
@@ -65,20 +72,27 @@ def get_addons_per_client(users_df, addon_whitelist, minimum_addons_count):
 
     def is_valid_addon(guid, addon):
         return not (
-            addon.is_system or
-            addon.app_disabled or
-            addon.type != "extension" or
-            addon.user_disabled or
-            addon.foreign_install or
-            guid not in addon_whitelist
+            addon.is_system
+            or addon.app_disabled
+            or addon.type != "extension"
+            or addon.user_disabled
+            or addon.foreign_install
+            or guid not in addon_whitelist
         )
 
     # Create an add-ons dataset un-nesting the add-on map from each
     # user to a list of add-on GUIDs. Also filter undesired add-ons.
     return (
-        users_df.rdd
-        .map(lambda p: (p["client_id"],
-             [guid for guid, data in p["active_addons"].items() if is_valid_addon(guid, data)]))
+        users_df.rdd.map(
+            lambda p: (
+                p["client_id"],
+                [
+                    guid
+                    for guid, data in p["active_addons"].items()
+                    if is_valid_addon(guid, data)
+                ],
+            )
+        )
         .filter(lambda p: len(p[1]) > minimum_addons_count)
         .toDF(["client_id", "addon_ids"])
     )
@@ -101,19 +115,14 @@ def compute_clusters(addons_df, num_clusters, random_seed):
 
     # Run the pipeline and compute the results.
     model = pipeline.fit(addons_df)
-    return (
-        model
-        .transform(addons_df)
-        .select(["client_id", "prediction"])
-    )
+    return model.transform(addons_df).select(["client_id", "prediction"])
 
 
 def get_donor_pools(users_df, clusters_df, num_donors, random_seed=None):
     """ Samples users from each cluster.
     """
     cluster_population = clusters_df.groupBy("prediction").count().collect()
-    clusters_histogram =\
-        [(x["prediction"], x["count"]) for x in cluster_population]
+    clusters_histogram = [(x["prediction"], x["count"]) for x in cluster_population]
 
     # Sort in-place from highest to lowest populated cluster.
     clusters_histogram.sort(key=lambda x: x[0], reverse=False)
@@ -130,15 +139,14 @@ def get_donor_pools(users_df, clusters_df, num_donors, random_seed=None):
     # Sample the users in each cluster according to the proportions
     # and pass along the random seed if needed for tests.
     sampling_kwargs = {"seed": random_seed} if random_seed else {}
-    donor_df = clusters_df.sampleBy("prediction",
-                                    fractions=sampling_proportions,
-                                    **sampling_kwargs)
+    donor_df = clusters_df.sampleBy(
+        "prediction", fractions=sampling_proportions, **sampling_kwargs
+    )
     # Get the specific number of donors for each cluster and drop the
     # predicted cluster number information.
     current_sample_size = donor_df.count()
-    donor_pool_df = (
-        donor_df
-        .sample(False, float(num_donors) / current_sample_size, **sampling_kwargs)
+    donor_pool_df = donor_df.sample(
+        False, float(num_donors) / current_sample_size, **sampling_kwargs
     )
     return clusters, donor_pool_df
 
@@ -152,25 +160,28 @@ def get_donors(spark, num_clusters, num_donors, addon_whitelist, random_seed=Non
     # Perform clustering by using the add-on info.
     clusters = compute_clusters(addons_df, num_clusters, random_seed)
     # Sample representative ("donors") users from each cluster.
-    cluster_ids, donors_df =\
-        get_donor_pools(users_sample, clusters, num_donors, random_seed)
+    cluster_ids, donors_df = get_donor_pools(
+        users_sample, clusters, num_donors, random_seed
+    )
 
     # Finally, get the feature vectors for users that represent
     # each cluster. Since the "active_addons" in "users_sample"
     # are in a |MapType| and contain system add-ons as well, just
     # use the cleaned up list from "addons_df".
-    return cluster_ids, (
-        users_sample
-        .join(donors_df, 'client_id')
-        .drop('active_addons')
-        .join(addons_df, 'client_id', 'left')
-        .drop('client_id')
-        .withColumnRenamed('addon_ids', 'active_addons')
+    return (
+        cluster_ids,
+        (
+            users_sample.join(donors_df, "client_id")
+            .drop("active_addons")
+            .join(addons_df, "client_id", "left")
+            .drop("client_id")
+            .withColumnRenamed("addon_ids", "active_addons")
+        ),
     )
 
 
 def format_donors_dictionary(donors_df):
-    cleaned_records = donors_df.drop('prediction').collect()
+    cleaned_records = donors_df.drop("prediction").collect()
     # Convert each row to a dictionary.
     return [row.asDict() for row in cleaned_records]
 
@@ -196,8 +207,8 @@ def similarity_function(x, y):
     y_continuous_features = [safe_get(k, y, 0) for k in CONTINUOUS_FEATURES]
 
     # Here a larger distance indicates a poorer match between categorical variables.
-    j_d = (distance.hamming(x_categorical_features, y_categorical_features))
-    j_c = (distance.canberra(x_continuous_features, y_continuous_features))
+    j_d = distance.hamming(x_categorical_features, y_categorical_features)
+    j_c = distance.canberra(x_continuous_features, y_continuous_features)
 
     # Take the product of similarities to attain a univariate similarity score.
     # Add a minimal constant to prevent zero values from categorical features.
@@ -217,8 +228,9 @@ def generate_non_cartesian_pairs(first_rdd, second_rdd):
     return rdd1_with_indices.join(rdd2_with_indices).map(lambda p: p[1])
 
 
-def get_lr_curves(spark, features_df, cluster_ids, kernel_bandwidth,
-                  num_pdf_points, random_seed=None):
+def get_lr_curves(
+    spark, features_df, cluster_ids, kernel_bandwidth, num_pdf_points, random_seed=None
+):
     """ Compute the likelihood ratio curves for clustered clients.
 
     Work-flow followed in this function is as follows:
@@ -249,38 +261,40 @@ def get_lr_curves(spark, features_df, cluster_ids, kernel_bandwidth,
 
     for cluster_number in cluster_ids:
         # Pick the features for users belonging to the current cluster.
-        current_cluster_df = (
-            features_df
-            .where(col("prediction") == cluster_number)
-        )
+        current_cluster_df = features_df.where(col("prediction") == cluster_number)
         # Pick the features for users belonging to all the other clusters.
-        other_clusters_df = (
-            features_df
-            .where(col("prediction") != cluster_number)
-        )
+        other_clusters_df = features_df.where(col("prediction") != cluster_number)
 
-        logger.debug("Computing scores for cluster", extra={"cluster_id": cluster_number})
+        logger.debug(
+            "Computing scores for cluster", extra={"cluster_id": cluster_number}
+        )
 
         # Compares the similarity score between pairs of clients in the same cluster.
-        cluster_half_1, cluster_half_2 =\
-            current_cluster_df.rdd.randomSplit([0.5, 0.5], **random_split_kwargs)
+        cluster_half_1, cluster_half_2 = current_cluster_df.rdd.randomSplit(
+            [0.5, 0.5], **random_split_kwargs
+        )
         pair_rdd = generate_non_cartesian_pairs(cluster_half_1, cluster_half_2)
         intra_scores_rdd = pair_rdd.map(lambda r: similarity_function(*r))
         same_cluster_scores_rdd = same_cluster_scores_rdd.union(intra_scores_rdd)
 
         # Compares the similarity score between pairs of clients in different clusters.
-        pair_rdd =\
-            generate_non_cartesian_pairs(current_cluster_df.rdd, other_clusters_df.rdd)
+        pair_rdd = generate_non_cartesian_pairs(
+            current_cluster_df.rdd, other_clusters_df.rdd
+        )
         inter_scores_rdd = pair_rdd.map(lambda r: similarity_function(*r))
-        different_clusters_scores_rdd =\
-            different_clusters_scores_rdd.union(inter_scores_rdd)
+        different_clusters_scores_rdd = different_clusters_scores_rdd.union(
+            inter_scores_rdd
+        )
 
     # Determine a range of observed similarity values linearly spaced.
     all_scores_rdd = same_cluster_scores_rdd.union(different_clusters_scores_rdd)
     min_similarity = all_scores_rdd.min()
     max_similarity = all_scores_rdd.max()
-    lr_index = np.arange(min_similarity, max_similarity,
-                         float(abs(min_similarity - max_similarity)) / num_pdf_points)
+    lr_index = np.arange(
+        min_similarity,
+        max_similarity,
+        float(abs(min_similarity - max_similarity)) / num_pdf_points,
+    )
 
     # Kernel density estimate for the inter-cluster comparison scores.
     kd_dc = KernelDensity()
@@ -299,22 +313,26 @@ def get_lr_curves(spark, features_df, cluster_ids, kernel_bandwidth,
 
 
 @click.command()
-@click.option('--date', required=True)
-@click.option('--bucket', default='telemetry-parquet')
-@click.option('--prefix', default='taar/similarity/')
-@click.option('--num_clusters', default=20)
-@click.option('--num_donors', default=1000)
-@click.option('--kernel_bandwidth', default=0.35)
-@click.option('--num_pdf_points', default=1000)
-def main(date, bucket, prefix, num_clusters, num_donors, kernel_bandwidth, num_pdf_points):
-    spark = (SparkSession
-             .builder
-             .appName("taar_similarity")
-             .enableHiveSupport()
-             .getOrCreate())
+@click.option("--date", required=True)
+@click.option("--bucket", default="telemetry-parquet")
+@click.option("--prefix", default="taar/similarity/")
+@click.option("--num_clusters", default=20)
+@click.option("--num_donors", default=1000)
+@click.option("--kernel_bandwidth", default=0.35)
+@click.option("--num_pdf_points", default=1000)
+def main(
+    date, bucket, prefix, num_clusters, num_donors, kernel_bandwidth, num_pdf_points
+):
+    spark = (
+        SparkSession.builder.appName("taar_similarity")
+        .enableHiveSupport()
+        .getOrCreate()
+    )
 
     if num_donors < 100:
-        logger.warn("Less than 100 donors were requested.", extra={"donors": num_donors})
+        logger.warn(
+            "Less than 100 donors were requested.", extra={"donors": num_donors}
+        )
         num_donors = 100
 
     logger.info("Loading the AMO whitelist...")
@@ -324,13 +342,12 @@ def main(date, bucket, prefix, num_clusters, num_donors, kernel_bandwidth, num_p
 
     # Compute the donors clusters and the LR curves.
     cluster_ids, donors_df = get_donors(spark, num_clusters, num_donors, whitelist)
-    lr_curves = get_lr_curves(spark, donors_df, cluster_ids, kernel_bandwidth,
-                              num_pdf_points)
+    lr_curves = get_lr_curves(
+        spark, donors_df, cluster_ids, kernel_bandwidth, num_pdf_points
+    )
 
     # Store them.
     donors = format_donors_dictionary(donors_df)
-    store_json_to_s3(json.dumps(donors, indent=2), 'donors',
-                     date, prefix, bucket)
-    store_json_to_s3(json.dumps(lr_curves, indent=2), 'lr_curves',
-                     date, prefix, bucket)
+    store_json_to_s3(json.dumps(donors, indent=2), "donors", date, prefix, bucket)
+    store_json_to_s3(json.dumps(lr_curves, indent=2), "lr_curves", date, prefix, bucket)
     spark.stop()
--- tests/test_taar_similarity.py
+++ tests/test_taar_similarity.py
@@ -7,84 +7,92 @@ import pytest
 
 from mozetl.taar import taar_similarity
 from pyspark.sql.types import (
-    StructField, StructType, StringType,
-    LongType, BooleanType, ArrayType, MapType, Row
+    StructField,
+    StructType,
+    StringType,
+    LongType,
+    BooleanType,
+    ArrayType,
+    MapType,
+    Row,
 )
 
-longitudinal_schema = StructType([
-    StructField("client_id",             StringType(),  True),
-    StructField("normalized_channel",    StringType(),  True),
-    StructField("geo_city",              ArrayType(StringType(),  True)),
-    StructField("subsession_length",     ArrayType(LongType(),  True)),
-    StructField("os",                    StringType(),  True),
-    StructField(
-        "build",
-        ArrayType(
-            StructType(
-                [StructField("application_name",  StringType(),  True)]),
-            True),
-        True),
-    StructField("settings",              ArrayType(
-        StructType(
-            [StructField("locale",        StringType(),  True)]), True),
-        True),
-    StructField("active_addons",         ArrayType(
-        MapType(StringType(), StructType([
-            StructField("blocklisted",      BooleanType(), True),
-            StructField("type",             StringType(), True),
-            StructField("signed_state",     LongType(), True),
-            StructField("user_disabled",    BooleanType(), True),
-            StructField("app_disabled",     BooleanType(), True),
-            StructField("is_system",        BooleanType(), True),
-            StructField("foreign_install",  BooleanType(), True)
-        ]), True), True)),
-    StructField(
-        "places_bookmarks_count",
-        ArrayType(
-            StructType(
-                [StructField("sum",  LongType(),  True)]),
-            True),
-        True),
-    StructField(
-        "scalar_parent_browser_engagement_tab_open_event_count",
-        ArrayType(
-            StructType(
-                [StructField("value",  LongType(),  True)]),
-            True),
-        True),
-    StructField(
-        "scalar_parent_browser_engagement_total_uri_count",
-        ArrayType(
-            StructType(
-                [StructField("value",  LongType(),  True)]),
-            True),
-        True),
-    StructField(
-        "scalar_parent_browser_engagement_unique_domains_count",
-        ArrayType(
-            StructType(
-                [StructField("value",  LongType(),  True)]),
-            True),
-        True)
-    ])
+longitudinal_schema = StructType(
+    [
+        StructField("client_id", StringType(), True),
+        StructField("normalized_channel", StringType(), True),
+        StructField("geo_city", ArrayType(StringType(), True)),
+        StructField("subsession_length", ArrayType(LongType(), True)),
+        StructField("os", StringType(), True),
+        StructField(
+            "build",
+            ArrayType(
+                StructType([StructField("application_name", StringType(), True)]), True
+            ),
+            True,
+        ),
+        StructField(
+            "settings",
+            ArrayType(StructType([StructField("locale", StringType(), True)]), True),
+            True,
+        ),
+        StructField(
+            "active_addons",
+            ArrayType(
+                MapType(
+                    StringType(),
+                    StructType(
+                        [
+                            StructField("blocklisted", BooleanType(), True),
+                            StructField("type", StringType(), True),
+                            StructField("signed_state", LongType(), True),
+                            StructField("user_disabled", BooleanType(), True),
+                            StructField("app_disabled", BooleanType(), True),
+                            StructField("is_system", BooleanType(), True),
+                            StructField("foreign_install", BooleanType(), True),
+                        ]
+                    ),
+                    True,
+                ),
+                True,
+            ),
+        ),
+        StructField(
+            "places_bookmarks_count",
+            ArrayType(StructType([StructField("sum", LongType(), True)]), True),
+            True,
+        ),
+        StructField(
+            "scalar_parent_browser_engagement_tab_open_event_count",
+            ArrayType(StructType([StructField("value", LongType(), True)]), True),
+            True,
+        ),
+        StructField(
+            "scalar_parent_browser_engagement_total_uri_count",
+            ArrayType(StructType([StructField("value", LongType(), True)]), True),
+            True,
+        ),
+        StructField(
+            "scalar_parent_browser_engagement_unique_domains_count",
+            ArrayType(StructType([StructField("value", LongType(), True)]), True),
+            True,
+        ),
+    ]
+)
 
 default_sample = {
-    "client_id":             "client-id",
-    "normalized_channel":    "release",
+    "client_id": "client-id",
+    "normalized_channel": "release",
     "geo_city": ["Boston"],
     "subsession_length": [10],
     "os": "Windows",
-    "build": [{
-        "application_name":  "Firefox"
-    }],
-    "settings": [{
-        "locale":            "en-US"
-    }],
+    "build": [{"application_name": "Firefox"}],
+    "settings": [{"locale": "en-US"}],
     "active_addons": [],
     "places_bookmarks_count": [{"sum": 1}],
     "scalar_parent_browser_engagement_tab_open_event_count": [{"value": 2}],
     "scalar_parent_browser_engagement_total_uri_count": [{"value": 3}],
-    "scalar_parent_browser_engagement_unique_domains_count": [{"value": 4}]
+    "scalar_parent_browser_engagement_unique_domains_count": [{"value": 4}],
 }
 
 
@@ -93,7 +101,7 @@ def generate_data(dataframe_factory):
     return functools.partial(
         dataframe_factory.create_dataframe,
         base=default_sample,
-        schema=longitudinal_schema
+        schema=longitudinal_schema,
     )
 
 
@@ -108,10 +116,8 @@ def multi_clusters_df(generate_data):
                 "geo_city": ["Rome"],
                 "subsession_length": [3785],
                 "os": "Linux",
-                "settings": [{
-                    "locale": "it-IT"
-                }],
-            }
+                "settings": [{"locale": "it-IT"}],
+            },
         },
         {
             "count": 50,
@@ -119,10 +125,8 @@ def multi_clusters_df(generate_data):
                 "geo_city": ["London"],
                 "subsession_length": [1107],
                 "os": "MacOS",
-                "settings": [{
-                    "locale": "en-UK"
-                }],
-            }
+                "settings": [{"locale": "en-UK"}],
+            },
         },
         {
             "count": 50,
@@ -130,11 +134,9 @@ def multi_clusters_df(generate_data):
                 "geo_city": ["Boston"],
                 "subsession_length": [201507],
                 "os": "Windows",
-                "settings": [{
-                    "locale": "en-US"
-                }],
-            }
-        }
+                "settings": [{"locale": "en-US"}],
+            },
+        },
     ]
 
     sample_snippets = []
@@ -151,35 +153,35 @@ def multi_clusters_df(generate_data):
             for addon_id in range(0, 3):
                 addon_name = "var-{}-guid-{}".format(variation_id, addon_id)
                 variation["active_addons"][0][addon_name] = {
-                    "blocklisted":     False,
-                    "user_disabled":   False,
-                    "app_disabled":    False,
-                    "signed_state":    2,
-                    "type":            "extension",
+                    "blocklisted": False,
+                    "user_disabled": False,
+                    "app_disabled": False,
+                    "signed_state": 2,
+                    "type": "extension",
                     "foreign_install": False,
-                    "is_system":       False
+                    "is_system": False,
                 }
 
             # Additionally add a system addon.
             variation["active_addons"][0]["system-addon-guid"] = {
-                "blocklisted":     False,
-                "user_disabled":   False,
-                "app_disabled":    False,
-                "signed_state":    2,
-                "type":            "extension",
+                "blocklisted": False,
+                "user_disabled": False,
+                "app_disabled": False,
+                "signed_state": 2,
+                "type": "extension",
                 "foreign_install": False,
-                "is_system":       True
+                "is_system": True,
             }
 
             # Additionally add an AMO unlisted addon.
             variation["active_addons"][0]["unlisted-addon-guid"] = {
-                "blocklisted":     False,
-                "user_disabled":   False,
-                "app_disabled":    False,
-                "signed_state":    2,
-                "type":            "extension",
+                "blocklisted": False,
+                "user_disabled": False,
+                "app_disabled": False,
+                "signed_state": 2,
+                "type": "extension",
                 "foreign_install": False,
-                "is_system":       False
+                "is_system": False,
             }
 
             sample_snippets.append(variation)
@@ -199,7 +201,7 @@ def addon_whitelist():
 
 def test_non_cartesian_pairs(spark):
     TEST_DATA_1 = [1, 2, 3, 4]
-    TEST_DATA_2 = ['a', 'b', 'c', 'd']
+    TEST_DATA_2 = ["a", "b", "c", "d"]
 
     rdd1 = spark.sparkContext.parallelize(TEST_DATA_1)
     rdd2 = spark.sparkContext.parallelize(TEST_DATA_2)
@@ -214,17 +216,35 @@ def test_non_cartesian_pairs(spark):
 
 def test_similarity():
     UserDataRow = Row(
-        "normalized_channel", "geo_city", "subsession_length", "os", "locale",
-        "active_addons", "bookmark_count", "tab_open_count", "total_uri", "unique_tlds"
+        "normalized_channel",
+        "geo_city",
+        "subsession_length",
+        "os",
+        "locale",
+        "active_addons",
+        "bookmark_count",
+        "tab_open_count",
+        "total_uri",
+        "unique_tlds",
     )
 
-    test_user_1 = UserDataRow("release", "Boston", 10, "Windows", "en-US", [], 1, 2, 3, 4)
-    test_user_2 = UserDataRow("release", "notsoB", 10, "swodniW", "SU-ne", [], 1, 2, 3, 4)
-    test_user_3 = UserDataRow("release", "Boston", 0, "Windows", "en-US", [], 0, 0, 0, 0)
-    test_user_4 = UserDataRow("release", "notsoB", 0, "swodniW", "SU-ne", [], 0, 0, 0, 0)
+    test_user_1 = UserDataRow(
+        "release", "Boston", 10, "Windows", "en-US", [], 1, 2, 3, 4
+    )
+    test_user_2 = UserDataRow(
+        "release", "notsoB", 10, "swodniW", "SU-ne", [], 1, 2, 3, 4
+    )
+    test_user_3 = UserDataRow(
+        "release", "Boston", 0, "Windows", "en-US", [], 0, 0, 0, 0
+    )
+    test_user_4 = UserDataRow(
+        "release", "notsoB", 0, "swodniW", "SU-ne", [], 0, 0, 0, 0
+    )
     # The following user contains a None value for "total_uri" and geo_city
     # (categorical feature). The latter should never be possible, but let's be cautious.
-    test_user_5 = UserDataRow("release", None, 10, "swodniW", "SU-ne", [], 1, None, 3, 4)
+    test_user_5 = UserDataRow(
+        "release", None, 10, "swodniW", "SU-ne", [], 1, None, 3, 4
+    )
 
     # Identical users should be very close (0 distance) and the result must not
     # be a Numpy number.
@@ -233,10 +253,14 @@ def test_similarity():
     assert np.isclose(similarity_result, 0.0)
     # Users with completely different categorical features but identical
     # continuous features should be slightly different.
-    assert np.isclose(taar_similarity.similarity_function(test_user_1, test_user_2), 0.001)
+    assert np.isclose(
+        taar_similarity.similarity_function(test_user_1, test_user_2), 0.001
+    )
     # Users with completely different continuous features but identical
     # categorical features should be very close.
-    assert np.isclose(taar_similarity.similarity_function(test_user_1, test_user_3), 0.0)
+    assert np.isclose(
+        taar_similarity.similarity_function(test_user_1, test_user_3), 0.0
+    )
     # Completely different users should be far away.
     assert taar_similarity.similarity_function(test_user_1, test_user_4) >= 1.0
     # Partial user information should not break the similarity function.
@@ -262,7 +286,9 @@ def test_compute_donors(spark, addon_whitelist, multi_clusters_df):
 
     # Perform the clustering on our test data. We expect
     # 3 clusters out of this and 10 donors.
-    _, donors_df = taar_similarity.get_donors(spark, 3, 10, addon_whitelist, random_seed=42)
+    _, donors_df = taar_similarity.get_donors(
+        spark, 3, 10, addon_whitelist, random_seed=42
+    )
     donors = taar_similarity.format_donors_dictionary(donors_df)
 
     # Even if we requested 10 donors, it doesn't mean we will receive
@@ -279,8 +305,15 @@ def test_compute_donors(spark, addon_whitelist, multi_clusters_df):
 
         # The generated data must have all the required fields.
         REQUIRED_FIELDS = [
-            "geo_city", "subsession_length", "locale", "os", "bookmark_count",
-            "tab_open_count", "total_uri", "unique_tlds", "active_addons"
+            "geo_city",
+            "subsession_length",
+            "locale",
+            "os",
+            "bookmark_count",
+            "tab_open_count",
+            "total_uri",
+            "unique_tlds",
+            "active_addons",
         ]
         donor = cluster_donors[0]
         for f in REQUIRED_FIELDS:
