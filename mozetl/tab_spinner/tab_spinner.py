8da4cb951d5c2afb25ddfacd29b519667c8a80f3
William Lachance <wrlach@gmail.com>
2019-02-05 15:50:02 -0500

    Fix tab-spinner click command (#310)
    We were passing the wrong type of spark object to the actual etl
    code.

--- mozetl/tab_spinner/tab_spinner.py
+++ mozetl/tab_spinner/tab_spinner.py
@@ -6,7 +6,7 @@ from generate_counts import run_spinner_etl
 @click.command()
 def main():
     spark = SparkSession.builder.appName("long_tab_spinners").getOrCreate()
-    run_spinner_etl(spark)
+    run_spinner_etl(spark.sparkContext)
 
 
 if __name__ == "__main__":
