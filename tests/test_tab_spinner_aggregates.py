f2671b33e9221ae022fcb41b1c093ce47e1d0efa
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-02-25 10:35:26 -0800

    Add failing tests for tab_spinner

--- tests/test_tab_spinner_aggregates.py
+++ tests/test_tab_spinner_aggregates.py
@@ -1,8 +1,12 @@
 import pytest
 import json
 import pandas as pd
+import boto3
+from click.testing import CliRunner
+from moto import mock_s3
 
 from mozetl.tab_spinner.utils import get_short_and_long_spinners
+from mozetl.tab_spinner import tab_spinner
 
 
 def create_row():
@@ -32,3 +36,32 @@ def test_simple_transform(simple_rdd, spark_context):
     for k, v in expected.items():
         for build_id, series in v.items():
             assert all(result[k][build_id] == series)
+
+
+@mock_s3
+def test_tab_spinner_main(monkeypatch, simple_rdd):
+    bucket = "telemetry-public-analysis-2"
+    prefix = "spinner-severity-generator/data/severities_by_build_id_nightly.json"
+
+    conn = boto3.resource("s3", region_name="us-west-2")
+    conn.create_bucket(Bucket=bucket)
+
+    class Dataset:
+        @staticmethod
+        def from_source(*args, **kwargs):
+            return Dataset()
+
+        def where(self, *args, **kwargs):
+            return self
+
+        def records(self, *args, **kwargs):
+            return simple_rdd
+
+    monkeypatch.setattr("mozetl.tab_spinner.generate_counts.Dataset", Dataset)
+
+    runner = CliRunner()
+    result = runner.invoke(tab_spinner.main, [])
+    assert result.exit_code == 0
+
+    # should not raise a botocore exception
+    conn.Object(bucket, prefix).load()
