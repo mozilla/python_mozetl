cfba4b2ebb50dc6a2186fc2e9211cac88fcc5cb7
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-08-05 15:58:46 -0700

    Bug 1571565 - Decommission retention_v1 dataset

--- mozetl/cli.py
+++ mozetl/cli.py
@@ -4,7 +4,6 @@ from mozetl.addon_aggregates import addon_aggregates
 from mozetl.clientsdaily import rollup as clientsdaily
 from mozetl.engagement.churn import job as churn_job
 from mozetl.engagement.churn_to_csv import job as churn_to_csv_job
-from mozetl.engagement.retention import job as retention_job
 from mozetl.experimentsdaily import rollup as experimentsdaily
 from mozetl.landfill import sampler as landfill_sampler
 from mozetl.maudau import maudau
@@ -34,7 +33,6 @@ entry_point.add_command(churn_job.main, "churn")
 entry_point.add_command(churn_to_csv_job.main, "churn_to_csv")
 entry_point.add_command(clientsdaily.main, "clients_daily")
 entry_point.add_command(experimentsdaily.main, "experiments_daily")
-entry_point.add_command(retention_job.main, "retention")
 entry_point.add_command(search_aggregates_click, "search_aggregates")
 entry_point.add_command(search_clients_daily_click, "search_clients_daily")
 entry_point.add_command(bookmark_validation.main, "sync_bookmark_validation")
