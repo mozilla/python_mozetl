5756722cd5820aa988034cf1fbc620b619af0343
Ben Wu <benjaminwu124@gmail.com>
2020-12-15 12:23:57 -0500

    Port symbolication jobs from databricks (#353)

---
+++ bin/mozetl-submit-dataproc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Test a pyspark job on dataproc
+# This script assumes a cluster was already created with the given name
+# and the correct configuration
+# See mozetl/graphics/create_cluster.sh for an example
+
+# Example Usage: ./mozetl-submit-dataproc graphics-test-cluster mozetl/graphics/graphics_telemetry_dashboard.py
+
+if [[ -z $1 ]]; then
+    echo '$1 (cluster name) not defined'
+    exit 1
+fi
+if [[ -z $2 ]]; then
+    echo '$2 (job path) not defined'
+    exit 1
+fi
+
+gcloud dataproc jobs submit pyspark $2 \
+    --cluster=$1 \
+    --region=us-central1 \
+    --jars 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'
--- mozetl/graphics/create_trends_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Test the graphics_telemetry_trends.py job
-# This assumes a cluster was created via create_cluster.sh
-
-gcloud dataproc jobs submit pyspark graphics_telemetry_trends.py \
-    --cluster=graphics-spark-test \
-    --region=us-central1 \
-    --jars 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'
