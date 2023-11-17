2e0d78ced6b6d12ee0068620cd23026f56904351
Alekhya Kommasani <akommasani@mozilla.com>
2023-11-17 13:57:13 -0500

    fix ci lint issue

--- mozetl/bhr_collection/bhr_collection.py
+++ mozetl/bhr_collection/bhr_collection.py
@@ -1144,7 +1144,11 @@ def write_file(name, stuff, config):
         blob.upload_from_filename(gzfilename, **extra_args)
         if config["uuid"] is not None:
             gcs_key = (
-                "bhr/test_folder/hang_aggregates/" + name + "_" + config["uuid"] + ".json"
+                "bhr/test_folder/hang_aggregates/"
+                + name
+                + "_"
+                + config["uuid"]
+                + ".json"
             )
             storage_client = storage.Client()
             bucket = storage_client.bucket(bucket_name)
