539e01bd6bc4388fe31805dda9924e7a40320aa5
Alekhya Kommasani <akommasani@mozilla.com>
2023-11-21 21:06:06 -0500

    Add metadata to bhr_collection

--- mozetl/bhr_collection/create_cluster.sh
+++ mozetl/bhr_collection/create_cluster.sh
@@ -14,7 +14,7 @@ fi
 gcloud dataproc clusters create bhr-collection \
     --image-version=1.5 \
     --region=us-central1 \
-    --metadata='PIP_PACKAGES=boto3==1.16.20 click==7.1.2' \
+    --metadata='PIP_PACKAGES=boto3==1.16.20 click==7.1.2 google-cloud-storage==2.7.0' \
     --num-workers=5 \
     --worker-machine-type='n2-highmem-4' \
     --properties "core:fs.s3.awsAccessKeyId=$AWS_ACCESS_KEY_ID,core:fs.s3.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY,spark-env:AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID,spark-env:AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
